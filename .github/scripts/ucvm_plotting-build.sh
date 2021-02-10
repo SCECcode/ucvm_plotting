3c39be5c63a4cc05229260d94253da8497f67b6d
Mei-Hui Su <mei@MeiPro.local>
2021-02-10 00:33:30 -0800

    add description

--- .github/scripts/ucvm_plotting-build.sh
+++ .github/scripts/ucvm_plotting-build.sh
@@ -2,9 +2,9 @@
 
 source $UCVM_INSTALL_PATH/conf/ucvm_env.sh
 
-tmp=`uname -s`
-
-./unpack-dist
-
+echo "--- Show where ucvm_query is from :"
 which ucvm_query 
+echo "--- Show ucvm built info :"
 ucvm_query -H
+echo "--- Unpac ucvm_plotting :"
+./unpack-dist
