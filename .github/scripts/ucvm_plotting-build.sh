7e19d1287b64dee58feced3a4baa7a44d9b88d93
Mei-Hui Su <mei@MeiPro.local>
2021-02-10 12:15:30 -0800

    test models

--- .github/scripts/ucvm_plotting-build.sh
+++ .github/scripts/ucvm_plotting-build.sh
@@ -4,8 +4,11 @@ source $UCVM_INSTALL_PATH/conf/ucvm_env.sh
 
 echo "--- Show where ucvm_query is from :"
 which ucvm_query 
+
 echo "--- Show ucvm built info :"
-ucvm_query -H
+which installed_models.py
+installed_models.py
+
 echo "--- Unpac ucvm_plotting :"
 conda install -y matplotlib basemap basemap-data-hires
 ./unpack-dist
