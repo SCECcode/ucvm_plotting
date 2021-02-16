7ba2bb68a65970d4142c18c6a1274e8badcec83d
Mei-Hui Su <mei@MeiPro.local>
2021-02-16 15:13:41 -0800

    fix exception format in common.py

--- pycvm/common.py
+++ pycvm/common.py
@@ -38,7 +38,7 @@ except:
 #  Basemap is required.
 try:
     from mpl_toolkits import basemap
-except StandardError, e:
+except Exception as e:
     print("ERROR: Basemap must be installed on your system in order to generate these plots.")
     print(e)
     exit(1)
