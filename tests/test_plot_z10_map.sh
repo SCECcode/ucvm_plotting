4d8add98ed705e0970ef99c32760484c33ae48ff
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2024-11-01 00:00:08 +0000

    add another test

---
+++ tests/test_plot_z10_map.sh
@@ -0,0 +1,23 @@
+#
+#
+
+LAT1=35.02
+LON1=-126.4
+LAT2=41.50
+LON2=-118.8
+SPACING=0.1
+MODEL=sfcvm 
+
+rm -rf sfcvm_z10_data.bin sfcvm_z10_meta.json sfcvm_z10.png
+rm -rf sfcvm_d_z10.png
+
+## generate data and plot
+time plot_z10_map.py -b ${LAT1},${LON1} -u ${LAT2},${LON2} -c ${MODEL} -a s -s ${SPACING} -o ${MODEL}_z10.png -i ${UCVM_INSTALL_PATH} -t "Z10 ${MODEL}"
+
+## generate plot from given data
+time plot_z10_map.py -b ${LAT1},${LON1} -u ${LAT2},${LON2} -c ${MODEL} -a s -s ${SPACING} -o ${MODEL}_d_z10.png -i ${UCVM_INSTALL_PATH} -t "Z10 ${MODEL}" -f ${MODEL}_z10_data.bin
+
+## convert to csv
+## using ucvm_metadata_utilities
+#./ucvm_horizontal_slice2csv.py sfcvm_z10_data.bin sfcvm_z10_meta.json 
+
--- tests/test_plot_z25_map.sh
+++ tests/test_plot_z25_map.sh
@@ -9,6 +9,7 @@ SPACING=0.1
 MODEL=sfcvm 
 
 rm -rf sfcvm_z25_data.bin sfcvm_z25_meta.json sfcvm_z25.png
+rm -rf sfcvm_d_z25.png
 
 ## generate data and plot
 time plot_z25_map.py -b ${LAT1},${LON1} -u ${LAT2},${LON2} -c ${MODEL} -a s -s ${SPACING} -o ${MODEL}_z25.png -i ${UCVM_INSTALL_PATH} -t "Z25 ${MODEL}"
@@ -18,5 +19,5 @@ time plot_z25_map.py -b ${LAT1},${LON1} -u ${LAT2},${LON2} -c ${MODEL} -a s -s $
 
 ## convert to csv
 ## using ucvm_metadata_utilities
-#./ucvm_horizontal_slice2csv.py sfcvm_z25_01_data.bin sfcvm_z25_01_meta.json 
+#./ucvm_horizontal_slice2csv.py sfcvm_z25_data.bin sfcvm_z25_meta.json 
 
