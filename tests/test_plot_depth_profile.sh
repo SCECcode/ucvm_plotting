f47c55cbf80e3cfa43b439374fa0107fb3ff1fe5
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2024-11-05 17:28:26 +0000

    fix test

--- tests/test_plot_depth_profile.sh
+++ tests/test_plot_depth_profile.sh
@@ -6,13 +6,15 @@ MODEL=sfcvm
 LAT1=38.1729
 LON1=-121.4332
 START=0
-END=3000
-STEP=100
+END=50000
+STEP=80
 
 rm -rf ${MODEL}_d_d_vs.png ${MODEL}_d_dd_vs.png
 rm -rf ${MODEL}_d_vs_data.bin ${MODEL}_d_d_vs_data.bin 
 rm -rf ${MODEL}_d_vs_meta.json ${MODEL}_d_d_vs_meta.json 
 
+
+# plot_depth_profile.py -d vs -c ${MODEL} -o ${MODEL}_d_vs.png -i $UCVM_INSTALL_PATH -C "vertical profile vs ${MODEL}" -s ${LAT1},${LON1} -b ${START} -e ${END} -v ${STEP}
 
 time plot_depth_profile.py -d vs -c ${MODEL} -o ${MODEL}_d_vs.png -i $UCVM_INSTALL_PATH -C "vertical profile vs ${MODEL}" -s ${LAT1},${LON1} -b ${START} -e ${END} -v ${STEP}
 
@@ -30,5 +32,5 @@ diff ${MODEL}_d_d_vs_meta.json  ${MODEL}_d_vs_meta.json
 ## convert to csv
 ## using ucvm_metadata_utilities
 ./ucvm_vertical_profile2csv.py ${MODEL}_d_vs_matprops.json ${MODEL}_d_vs_meta.json
-./ucvm_vertical_profile2csv.py ${MODEL}_vs_matprops.json ${MODEL}_vs_meta.json
+./ucvm_vertical_profile2csv.py ${MODEL}_d_d_vs_matprops.json ${MODEL}_d_d_vs_meta.json
 
