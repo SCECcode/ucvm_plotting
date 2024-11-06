5970307be698cea617b028499a2a4c65d7ea803d
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2024-11-06 08:25:23 +0000

    extend vs30_etree_map to include depth and data_type

---
+++ tests/test_plot_vs30_etree_map.sh
@@ -0,0 +1,33 @@
+#
+# test_plot_vs30_etree_map.sh
+#
+
+MODEL=1d
+LAT1=37.2635
+LON1=-122.1945
+LAT2=38.3315
+LON2=-121.1536
+
+rm -rf ${MODEL}_h_vs.png ${MODEL}_h_dd_vs.png
+rm -rf ${MODEL}_h_vs_data.bin ${MODEL}_h_d_vs_data.bin 
+rm -rf ${MODEL}_h_vs_meta.json ${MODEL}_h_d_vs_meta.json 
+
+## generate data and plot
+time plot_vs30_etree_map.py -s 0.1 -c ${MODEL} -a sd -o ${MODEL}_vs30.png -i $UCVM_INSTALL_PATH -t "Thompson Caliornia vs30 Model v2020 thru UCVM" -b ${LAT1},${LON1} -u ${LAT2},${LON2}
+
+## generate data only
+time plot_vs30_etree_map.py -s 0.1 -c ${MODEL} -a sd -o ${MODEL}_vs30_d.png -i $UCVM_INSTALL_PATH -t "Thompson Caliornia vs30 Model v2020 thru UCVM" -b ${LAT1},${LON1} -u ${LAT2},${LON2} -S
+
+## generate plot from given data
+-f ${MODEL}_vs30_d_data.bin
+time plot_vs30_etree_map.py -s 0.1 -c ${MODEL} -a sd -o ${MODEL}_vs30_dd.png -i $UCVM_INSTALL_PATH -t "Thompson Caliornia vs30 Model v2020 thru UCVM" -b ${LAT1},${LON1} -u ${LAT2},${LON2} -f ${MODEL}_vs30_d_data.bin
+
+## should be about the same
+diff ${MODEL}_vs30_meta.json  ${MODEL}_vs30_dd_meta.json
+
+
+## convert to csv
+## using ucvm_metadata_utilities
+./ucvm_horizontal_slice2csv.py ${MODEL}_vs30_data.bin ${MODEL}_vs30_meta.json
+./ucvm_horizontal_slice2csv_line.py ${MODEL}_vs30_dd_data.bin ${MODEL}_vs30_dd_meta.json
+
