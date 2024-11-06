15e7b578be5becacb236a43e54373de66f851b2c
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2024-11-06 16:24:38 +0000

    tweak tests

--- tests/test_plot_vs30_etree_map.sh
+++ tests/test_plot_vs30_etree_map.sh
@@ -8,9 +8,9 @@ LON1=-122.1945
 LAT2=38.3315
 LON2=-121.1536
 
-#rm -rf ${MODEL}_vs30.png ${MODEL}_vs30_dd.png 
-#rm -rf ${MODEL}_vs30_data.bin ${MODEL}_vs30_dd_data.bin ${MODEL}_vs30_d_data.bin 
-#rm -rf ${MODEL}_vs30_meta.json ${MODEL}_vs30_dd_meta.json ${MODEL}_vs30_d_meta.json 
+rm -rf ${MODEL}_vs30.png ${MODEL}_vs30_dd.png 
+rm -rf ${MODEL}_vs30_data.bin ${MODEL}_vs30_d_data.bin 
+rm -rf ${MODEL}_vs30_meta.json ${MODEL}_vs30_d_meta.json 
 
 ## generate data and plot
 time plot_vs30_etree_map.py -s 0.1 -c ${MODEL} -a sd -o ${MODEL}_vs30.png -i $UCVM_INSTALL_PATH -t "Thompson Caliornia vs30 Model v2020 thru UCVM" -b ${LAT1},${LON1} -u ${LAT2},${LON2}
@@ -28,5 +28,5 @@ diff ${MODEL}_vs30_meta.json  ${MODEL}_vs30_dd_meta.json
 ## convert to csv
 ## using ucvm_metadata_utilities
 ./ucvm_horizontal_slice2csv.py ${MODEL}_vs30_data.bin ${MODEL}_vs30_meta.json
-./ucvm_horizontal_slice2csv_line.py ${MODEL}_vs30_dd_data.bin ${MODEL}_vs30_dd_meta.json
+./ucvm_horizontal_slice2csv_line.py ${MODEL}_vs30_d_data.bin ${MODEL}_vs30_d_meta.json
 
